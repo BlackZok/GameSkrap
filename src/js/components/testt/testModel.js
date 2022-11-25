c23bfaf76bae0e6ee2590e77eb6e1b9acf0736b7
Romochka <romanahvlediani@gmail.com>
2022-11-25 21:01:44 +0300

    test

---
+++ src/js/components/testt/testModel.js
@@ -0,0 +1,16 @@
+export const generalTest = [
+
+    {
+
+        text_1: "Бебра ", 
+        text_2: " VN;JRG;WB;SBS ", 
+    
+    },
+    {
+
+        text_1: "Бебра ", 
+        text_2: " VN;JRG;WB;SBS ", 
+    
+    }
+
+]
