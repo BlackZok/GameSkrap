41d41dac6a84518a8b11f90d7b335b1220c34472
BlackZok <evgeniy.zok@mail.ru>
2022-12-02 19:27:54 +0300

    get_started

---
+++ src/js/components/headertex/headertexModel.js
@@ -0,0 +1,18 @@
+export const generalHeaderText = [
+  {
+    title: [
+      {text: 'Насосы'},
+      {text: 'Электрика'},
+      {text: 'Двигатели'}
+    ],
+    text: 'Заказать',
+  },
+]
+
+export const secondHeaderText = [
+  {
+    text1: 'HEPA фильтры',
+    text2: 'Теплообменники',
+    text3: 'Холодильное оборудование'
+  },
+]
