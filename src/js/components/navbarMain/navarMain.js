a59fb05f2c9575bb331281984ab6686cd5f11e54
Romochka <romanahvlediani@gmail.com>
2022-12-09 21:12:29 +0300

    apieapeg

--- src/js/components/navbarMain/navarMain.js
+++ src/js/components/navbarMain/navarMain.js
@@ -5,7 +5,7 @@ const navbarMain = document.querySelector('#navbarMain');
 let navbarMainHtml = generalnavbarMain.map((a) => {
    return `
   
-        <div class="col-md-4><p class="header-main__text"> ${a.text} </p></div>
+        <div class="col-md-4"><p class="header-main__text"> ${a.text} </p></div>
     `
 }).join(" ");
 
